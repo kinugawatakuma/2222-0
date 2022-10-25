e95a4c5c66f9148b4cb15452b287d1e640862010
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2022-10-25 10:07:25 +0900

    Create ex6-8.js

---
+++ ex6-8.js
@@ -0,0 +1,9 @@
+function sayhello68(){
+ALERT('Hello,'document.getElementByld('who68').innerHTML+'!');
+}
+function taro68(){
+document.getElementByld('who68').innerHTML='太郎';
+}
+function hanako68(){
+   dodument.getElementByld('who68).innerHTML='花子'
+                           }
