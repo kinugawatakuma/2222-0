a3667b6b2c44d3571558f93e651d338ce48c9f24
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2022-11-08 09:08:54 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,9 +1,9 @@
 function sayhello68(){
-ALERT('Hello,'document.getElementById('who68').innerHTML+'!');
+alert('Hello,'document.getElementById('who68').innerHTML+'!');
 }
 function taro68(){
 document.getElementById('who68').innerHTML='太郎';
 }
 function hanako68(){
-   dodument.getElementById('who68).innerHTML='花子'
+   dodument.getElementById('who68).innerHTML='花子';
                            }
