578e79be457ab96bf02798439ea65c83818ddb20
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2022-11-15 10:21:02 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function hennkou() {document.getElementById('mizuta').innerHTML='<img src="./image/mizuta2.jpg" alt=水田２>';}
+function hennkou() {document.getElementById('mizuta').innerHTML='<img src="./image/mizuta2.jpg" width="300" height="225" alt=水田２>';}
