e4f646f12d2963fa7db270ff1b1bb499b5f6db13
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2022-12-13 09:56:44 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,4 @@
 function imgchange1() {
-document.getElementById("logo").src = './image/jo1.jpg';
+document.getElementById("logo").src = './image/josai.jpg';
 function imgchange2() {
 document.getElementById("logo").src = './image/jo1.jpg';
