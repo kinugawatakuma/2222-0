83a831db1eafe5665a1ed03f3dcebb9c108b920c
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2023-01-17 13:30:31 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,2 @@
-function imgchange1() {
-document.getElementById("logo").src = './image/josai.jpg';
-  }
-function imgchange2() {
-document.getElementById("logo").src = './image/jo1.jpg';
-}
+function imgchange1() {document.getElementById("logo").src = './image/josai.jpg';}
+function imgchange2() {document.getElementById("logo").src = './image/jo1.jpg';}
