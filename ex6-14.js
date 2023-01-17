3a529503561d83f7ae09e59a1a71342d312707bc
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2023-01-17 12:00:22 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,4 @@
-function hyoji14() {document.getElementById('josai14').innerHTML='<img src="./image/bulboff.gif" width="300" height="225" alt=水田２>'setTimeout('imgchange14()',3000);}
+function hyoji14() {document.getElementById('josai14').innerHTML='<img src="./image/bulboff.gif" width="300" height="225" alt=水田２>';}
+setTimeout('imgchange14()',3000)
 
 
