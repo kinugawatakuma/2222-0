ca95d6ebc69197b91a059584fd811d9058b784ce
kinugawatakuma <103973955+kinugawatakuma@users.noreply.github.com>
2023-01-17 10:42:08 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,1 +1,2 @@
-;setTimeout('imgchange6()',3000)
+function hyoji14() {document.getElementById('josai14').innerHTML='<img src="./image/bulboff.gif" width="300" height="225" alt=水田２>'
+  ;setTimeout('imgchange14()',3000)}
